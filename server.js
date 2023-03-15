5a474dd297fb10bd17cd894c08b496ff36f2a7fd
Le Minh Duc <79952214+samytythb@users.noreply.github.com>
2023-03-15 07:48:48 +0700

    update

--- server.js
+++ server.js
@@ -3,10 +3,17 @@ const app = express();
 const mongoose = require("mongoose");
 const router = require("./routes/index");
 const video = require("./routes/video");
-mongoose.connect("mongodb://localhost:27017/MyProject", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  "mongodb+srv://samytythb:leduc2821@cluster0.9gvnvqw.mongodb.net/MyProject?retryWrites=true&w=majority",
+  (req, res) => {
+    if (req) {
+      return console.log("Fail");
+    } else {
+      return console.log("Success");
+    }
+  },
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
 app.use(express.urlencoded({ extend: true }));
 app.use(express.static("public"));
 app.set("view engine", "pug");
